bed3334417aa6ef4364786f163e11fd2c320aed2
Florian Haas <florian@hastexo.com>
2016-01-08 12:34:39 +0100

    Simplify and use auto-target.js

--- static/js/auto-target.js
+++ static/js/auto-target.js
@@ -37,8 +37,7 @@
  */
 
 (function() {
-    var hostname = window.location.host;
-    hostname = hostname.split('.').slice(-2).join('.');
+    var hostname = window.location.hostname;
     var new_tab = true;
     var set_icon = true;
     for (var links = document.links, i = 0, a; a = links[i]; i++) {
@@ -47,7 +46,7 @@
                 a.target = '_blank';
             if (set_icon)
                 a.innerHTML +=
-                    '<i class="fa fa-external-link fa-1 external-link-margin" />';
+                    '&nbsp;<i class="fa fa-external-link fa-1 external-link-margin" />';
         }
     }
 })();
